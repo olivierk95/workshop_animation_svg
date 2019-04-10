ea5bc3017036f3604763db13bd8e58bbd2737c91
olivierk95 <keutgensolivier@gmail.com>
2019-04-10 22:42:12 +0200

    structure des documents

---
+++ script.js
@@ -0,0 +1,10 @@
+import anime from './animejs/lib/anime.es.js';
+
+// --- EXERCICE 8 ---
+
+anime({
+    // Va dans https://animejs.com/ pour créer ta propre animation en Javascript
+});
+
+// --- EXERCICE 9 ---
+// Anime le paysage en javascript si nécessaire
